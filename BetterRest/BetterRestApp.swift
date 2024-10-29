116290f45f01bd2d8aabfb7addbb70fd7da72a59
David Ferreira <81036027+DavidFerrs@users.noreply.github.com>
2024-10-29 12:03:53 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by David Ferreira on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
